eb1b32624e25efb8f39d9da598dbb6ca09dc5d6d
mehmet tuğrul kaan <61920968+tugrullkaan@users.noreply.github.com>
2022-03-14 13:02:28 +0300

    JavaTravelBook
    JavaTravelBook with LearningJava

---
+++ app/src/main/java/com/mtugrulkaan/javatravelbook/PlaceDatabase.java
@@ -0,0 +1,9 @@
+package com.mtugrulkaan.javatravelbook;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Place.class}, version = 1)
+public abstract class PlaceDatabase extends RoomDatabase {
+    public abstract PlaceDao placeDao();
+}
